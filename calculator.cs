7f24bc84477a9b4e8b1bc9cd7c25eb7ba0d515b4
Boybejmk <boybejmk@students.amikom.ac.id>
2023-03-13 17:04:04 +0700

    menambahkan fungsi utama

--- calculator.cs
+++ calculator.cs
@@ -1,7 +1,17 @@
 class calculator
 {
 	static void main(string[]args)
-	{}
+	{
+		int a = 10;
+		int b = 20;
+		
+		console.writeline("hasil penambahan: {0} + {1} = {2}",a, b, penambahan (a,b));
+		console.writeline("hasil pembagian: {0} / {1} = {2}",a, b, pembagian (a,b));
+		console.writeline("hasil pengurangan: {0} + {1} = {2}",a, b, pengurangan (a,b));
+		
+		console.writeline("\ntekan sembarang untuk keluar");
+		console.readkey();
+	}
 	static int penambahan (int a, int b)
 	{
 		return a+b
@@ -9,6 +19,10 @@ class calculator
 	static int pengurangan (int a, int b)
 	{
 		return a-b
+	}
+	static int pembagian (int a, int b)
+	{
+		return a/b
 	}
 	
 }
